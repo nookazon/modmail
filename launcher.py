70a53c950eca9eaf90c60521f52b3f030b1bfdb9
Daniel Luu <danielqluu@gmail.com>
2021-01-25 14:33:46 -0500

    pull from environ

--- launcher.py
+++ launcher.py
@@ -146,7 +146,7 @@ class Main:
         return None
 
     async def event_handler(self):
-        self.redis = await aioredis.create_pool("redis://localhost", minsize=1, maxsize=2)
+        self.redis = await aioredis.create_pool(config.redis_url, minsize=1, maxsize=2)
         await self.redis.execute_pubsub("SUBSCRIBE", config.ipc_channel)
         channel = self.redis.pubsub_channels[bytes(config.ipc_channel, "utf-8")]
         while await channel.wait_message():
