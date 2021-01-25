042b2c67d91a08f90f67a8b30d4e398c66c208d8
Daniel Luu <danielqluu@gmail.com>
2021-01-25 14:37:00 -0500

    push config

---
+++ config.py
@@ -0,0 +1,110 @@
+import os
+from urllib.parse import urlparse
+
+db_info = urlparse(os.environ.get('DATABASE_URL'))
+
+# Bot's token
+token = os.environ.get('TOKEN')
+
+# Top.gg token
+topgg_token = ""
+
+# Discord Bots token
+dbots_token = ""
+
+# Discord Bot List token
+dbl_token = ""
+
+# Bots on Discord token
+bod_token = ""
+
+# Bots for Discord token
+bfd_token = ""
+
+# Discord Boats token
+dboats_token = ""
+
+# Sentry URL
+sentry_url = ""
+
+# Whether the bot is for testing, if true, stats and errors will not be posted
+testing = False
+
+# Redis Connection String
+redis_url = os.environ.get('REDIS_URL')
+
+# PUBSUB channel for Redis
+ipc_channel = ""
+
+# Postgres database credentials
+database = {
+    "database": db_info.path[1:],
+    "user": db_info.username,
+    "password": db_info.password,
+    "host": db_info.hostname,
+    "port": 5432,
+    "ssl": 'require'
+}
+
+# Number of clusters
+clusters = 1
+
+# Additional shards to launch
+additional_shards = 0
+
+# The default prefix for commands
+default_prefix = "="
+
+# The server to send tickets to, no confirmation messages if set
+default_server = None
+
+# Status of the bot
+activity = f"DM to Contact Staff | {default_prefix}help"
+
+# Whether or not to fetch all members
+fetch_all_members = True
+
+# The main bot owner
+owner = 000000000000000000
+
+# Bot owners that have access to owner commands
+owners = []
+
+# Bot admins that have access to admin commands
+admins = []
+
+# Cogs to load on startup
+initial_extensions = [
+    "cogs.admin",
+    "cogs.communication",
+    "cogs.configuration",
+    "cogs.core",
+    "cogs.direct_message",
+    "cogs.error_handler",
+    "cogs.events",
+    "cogs.general",
+    "cogs.miscellaneous",
+    "cogs.modmail_channel",
+    "cogs.owner",
+    "cogs.premium",
+    "cogs.snippet",
+]
+
+# Channels to send logs
+join_channel = None
+event_channel = None
+admin_channel = None
+
+# This is where patron roles are at
+main_server = None
+
+# Patron roles for premium servers
+premium1 = None
+premium3 = None
+premium5 = None
+
+# The colour used in embeds
+primary_colour = 0x1E90FF
+user_colour = 0x00FF00
+mod_colour = 0xFF4500
+error_colour = 0xFF0000
